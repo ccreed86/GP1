5ff6b5c40258c1caebc9f6cc52b1a49feb3bd141
RTAKA808 <106231361+RTAKA808@users.noreply.github.com>
2024-04-08 11:51:30 -0700

    added event listener for future button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,37 @@
-let jokeUrl=`https://v2.jokeapi.dev/joke/${category}?format=json`
+//let category==user input
+
+let category= 'christmas'
+let jokeUrl=`https://v2.jokeapi.dev/joke/${category}?format=json?blacklistFlags=nsfw,religious,political,racist,sexist,explicit`
 let gifyKeyR='fNQfgqsi1G5OnBPBlie4e1lN3wVCBTTk'
 let gifyUrl= `https://api.giphy.com/v1/gifs/search?api_key=${gifyKeyR}&q=${category}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`
 
 
+
+//target.addEventListener('click', function(event){
+
+// })
+
+
+
+
+
+function joke(){
 fetch(jokeUrl).then(function(response){   //fetching data from the api 
     return response.json(); 
 }).then(function(data){ 
     console.log("--------- joke request --------")
     console.log(data);})
+}
 
-    fetch(url).then(function(response){   //fetching data from the api 
+
+function gif(){
+    fetch(gifyUrl).then(function(response){   //fetching data from the api 
         return response.json(); 
-    }).then(function(data){ 
+    }).then(function(data2){ 
         console.log("--------- gif request --------")
-        console.log(data);})
-
+        console.log(data2);})
+    }
     //joke/ setup &delivery
+    //if for single (joke )and two part (setup/delivery)
+
+    // Any, Misc, Programming, Pun, Spooky, Christmas
